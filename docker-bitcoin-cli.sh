8fe6a78e40f2326f8aa25b83da4e01a7606ec7a7
Ubuntu <ubuntu@ip-172-31-43-44.ap-northeast-1.compute.internal>
2020-09-24 01:27:03 +0000

    update

---
+++ docker-bitcoin-cli.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo -E docker-compose exec -T bitcoind bitcoin-cli -datadir="/data" -rpcport=43782 -rpcpassword=${BITCOIND_RPC_PASS} -rpcuser=${BITCOIND_RPC_USER} "$@"
--- lncli.sh
+++
@@ -1,1 +0,0 @@
-docker-compose exec lnd lncli --tlscertpath=/data/tls.cert --macaroonpath=/data/chain/bitcoin/mainnet/admin.macaroon --rpcserver=localhost:32777 $1
